c67e313a9c7b78aa10cb6532825b980685b0bbb9
Emirhan İpek <emirrhanipek@gmail.com>
2025-10-03 14:07:40 +0300

    Initial Commit

---
+++ apple-frameworks-app/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  apple-frameworks-app
+//
+//  Created by Emirhan Ipek on 3.10.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ apple-frameworks-app/apple_frameworks_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  apple_frameworks_appApp.swift
+//  apple-frameworks-app
+//
+//  Created by Emirhan Ipek on 3.10.2025.
+//
+
+import SwiftUI
+
+@main
+struct apple_frameworks_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
