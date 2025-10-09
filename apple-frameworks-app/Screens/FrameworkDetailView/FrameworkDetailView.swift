e8c1edc1b29a4a9521fa7292539736b7217baf88
Emirhan İpek <emirrhanipek@gmail.com>
2025-10-09 15:12:21 +0300

    Hotfix #1

--- apple-frameworks-app/Screens/FrameworkDetailView/FrameworkDetailView.swift
+++ apple-frameworks-app/Screens/FrameworkDetailView/FrameworkDetailView.swift
@@ -10,13 +10,10 @@ import SwiftUI
 struct FrameworkDetailView: View {
     
     var framework: Framework
-    @Binding var isShowingDetailView: Bool
     @State private var isShowingSafariView = false
     
     var body: some View {
-        VStack {
-            Spacer()
-            
+        VStack {            
             FrameworkTitleView(framework: framework)
             
             Text(framework.description)
@@ -28,8 +25,12 @@ struct FrameworkDetailView: View {
             Button {
                 isShowingSafariView = true
             } label: {
-                AFButton(title: "Learn More")
+//                AFButton(title: "Learn More")
+                Label("Learn More", systemImage: "book.fill")
             }
+            .buttonStyle(.bordered)
+            .controlSize(.large)
+            .tint(.blue)
         }
         .sheet(isPresented: $isShowingSafariView, content: {
             SafariView(url: URL(string: framework.urlString) ?? URL(string: "www.apple.com")!)
@@ -38,5 +39,5 @@ struct FrameworkDetailView: View {
 }
 
 #Preview {
-    FrameworkDetailView(framework: MockData.sampleFramework, isShowingDetailView: .constant(false))
+    FrameworkDetailView(framework: MockData.sampleFramework)
 }
--- apple-frameworks-app/Screens/FrameworkGridView/FrameworkGridViewModel.swift
+++ apple-frameworks-app/Screens/FrameworkGridView/FrameworkGridViewModel.swift
@@ -10,12 +10,6 @@ import Combine
 
 final class FrameworkGridViewModel: ObservableObject {
     
-    var selectedFramework: Framework? {
-        didSet { isShowingDetailView = true }
-    }
-    
-    @Published var isShowingDetailView = false
-    
     let columns: [GridItem] = [GridItem(.flexible()),
                                GridItem(.flexible()),
                                GridItem(.flexible())]
